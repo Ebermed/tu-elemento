8f5198aedc17a3b5d4e7c6fd9a354ae0fc9abefd
Ebermed <soapymed@gmail.com>
2026-08-22 01:01:48 -0600

    Start clean Copy V3 preview release

--- v11/release.js
+++ v11/release.js
@@ -9,11 +9,11 @@
 
   var RELEASE = Object.freeze({
     major: 2,
-    minor: 1,
-    patch: 4,
+    minor: 2,
+    patch: 0,
     codename: CODENAMES[1],
-    label: 'V2 Broadway',
-    build: '2.1.4-broadway'
+    label: 'V2 Broadway · Copy V3 Preview',
+    build: '2.2.0-copy-v3-preview'
   });
 
   function nombreVersion(major, minor, patch) {
